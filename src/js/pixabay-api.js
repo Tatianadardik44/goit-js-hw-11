79d5d2010b47dcccd024bf8a45e7f7def0baf37c
Tatianadardik44 <dardiktatiana@gmail.com>
2024-04-07 12:52:56 +0200

    error /

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@
 
 
 export function objGallery(names) {
-    const  path = `https://pixabay.com/api`;
+    const  path = `https://pixabay.com/api/`;
     const API_KEY = `43257905-28a3b58ba6106b31a5e4f67d7`;
     const params = new URLSearchParams({
         key: API_KEY,
