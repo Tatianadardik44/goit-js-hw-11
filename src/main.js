5916ba1a70539262641c5ae62eeeffe15d072702
Tatianadardik44 <dardiktatiana@gmail.com>
2024-04-07 11:12:11 +0200

    add loader

--- src/main.js
+++ src/main.js
@@ -3,16 +3,18 @@ import "izitoast/dist/css/iziToast.min.css";
 import { objGallery } from "./js/pixabay-api";
 import { createMarkup } from "./js/render-functions";
 import { list } from "./js/render-functions";
-
+const loaderElement = document.querySelector(".loader");
 const formGallery = document.querySelector(".input-field");
 formGallery.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
     event.preventDefault();
     list.innerHTML = "";
+    loaderElement.classList.remove(".hidden")
     const names = event.target.elements.designation.value.trim();
     objGallery(names)
         .then(data => {
             if (names === '') {
+                loaderElement.classList.add(".hidden");
                 iziToast.show({
                     message: 'Sorry, there are no images matching your search query. Please try again!',
                     messageColor: 'white',
@@ -21,6 +23,7 @@ function handleSubmit(event) {
 
                 });
             } else {
+                  loaderElement.classList.add(".hidden");
                 list.insertAdjacentHTML("beforeend", createMarkup(data.hits));  
             }
         });
